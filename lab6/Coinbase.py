886f2f310e493f3c8ffa0967b6b4eafdba87db74
vvvakhula <158203801+sanesaumvole@users.noreply.github.com>
2024-05-19 21:33:14 +0300

    Coinbase.py

--- lab6/Coinbase.py
+++ lab6/Coinbase.py
@@ -21,7 +21,6 @@ def get_historical_data(product, start, end, granularity):
         print(f"Failed to retrieve data for {product} - {start} to {end}")
         return None
 
-
 def get_available_products():
     url = "https://api.pro.coinbase.com/products"
     response = requests.get(url)
@@ -33,19 +32,18 @@ def get_available_products():
         print("Failed to retrieve products")
         return None
 
-
 products = get_available_products()
 print("Available products:", products)
-selected_products = ['XTZ-EUR', 'LTC-BTC', 'BTC-USD']
+selected_products = ['BTC-USD', 'ETH-USD', 'XRP-USD']
 end_date = datetime.now()
-start_date_day = end_date - timedelta(days=1)
-start_date_month = end_date - timedelta(days=30)
+start_date_week = end_date - timedelta(days=7)
+start_date_month = end_date - timedelta(days=90)
 start_date_year = end_date - timedelta(days=365)
 
 dfs = {}
 for product in selected_products:
     dfs[product] = {
-        'day': get_historical_data(product, start_date_day, end_date, 3600),
+        'week': get_historical_data(product, start_date_week, end_date, 3600),
         'month': get_historical_data(product, start_date_month, end_date, 86400),
         'year': get_historical_data(product, start_date_year, end_date, 86400 * 30)
     }
@@ -53,13 +51,10 @@ for product in selected_products:
 fig, axes = plt.subplots(nrows=len(selected_products), ncols=3, figsize=(15, 10))
 
 for i, product in enumerate(selected_products):
-    for j, period in enumerate(['day', 'month', 'year']):
+    for j, period in enumerate(['week', 'month', 'year']):
         ax = axes[i, j]
         if dfs[product][period] is not None:
             df = dfs[product][period]
-            ax.plot(df.index, df['open'], label='Open')
-            ax.plot(df.index, df['high'], label='High')
-            ax.plot(df.index, df['low'], label='Low')
             ax.plot(df.index, df['close'], label='Close')
             ax.set_title(f'{product} - {period}')
             ax.legend()
